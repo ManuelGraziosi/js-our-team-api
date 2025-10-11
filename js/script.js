3816e936e9e45676009dc9e43a87ee154326bc4a
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-10-11 16:15:29 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata  in cui mostrare una card per ciascun componente.
+
+// Per recuperare i dati utilizza la seguente API:
+
+// https://boolean-teachers.github.io/mock/api/members/
+
+
