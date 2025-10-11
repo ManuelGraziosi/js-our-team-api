2b307f509758eec9b24bbd1054f22d7768f49934
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-10-11 16:16:23 +0200

    feat: grid generation using API data completed

--- js/script.js
+++ js/script.js
@@ -4,4 +4,34 @@
 
 // https://boolean-teachers.github.io/mock/api/members/
 
+// recupero la maniglia dell'elemento che mi forma la griglia
+const gridRowElem = document.getElementById("members-grid");
+
+// recupero dei dati dalla API
+axios.get("https://boolean-teachers.github.io/mock/api/members/")
+    .then((resp) => {
+        console.log(resp.data);
+        const teamMembers = resp.data;
+        let teamMembersHtmlGrid = "";
+        for (let i = 0; i < teamMembers.length; i++) {
+            const curMember = teamMembers[i];
+            const { name, role, email, img } = curMember;
+            teamMembersHtmlGrid += `
+          <div class="col-12 col-md-6 col-lg-4">
+              <div class="card mb-3">
+                  <img src="${img}"
+                        class="card-img-top"
+                        alt="${name}">
+                  <div class="card-body">
+                      <h5 class="card-title">${name}</h5>
+                      <p class="card-text">${role}</p>
+                      <a href="mailto:${email}"
+                          class="card-text"><small class="text-muted">${email}</small></a>
+                  </div>
+              </div>
+          </div>
+          `;
+        }
+        gridRowElem.innerHTML = teamMembersHtmlGrid;
+    })
 
