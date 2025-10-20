41ae1c15c667979642b5c4e9ba27e4ce85f94997
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-10-20 20:34:42 +0200

    feat: bonus ese recipe by tag (wip)

---
+++ bonus/js/script.js
@@ -0,0 +1,73 @@
+/**
+ * recuperare i tag delle ricette e poi far comparire le card solo 
+ * dei tag selezionati
+ * 
+ *  */
+
+const mainElem = document.getElementById("main");
+const selectElem = document.getElementById("tagsList");
+const gridElem = document.getElementById("grid");
+const formElem = document.getElementById("form");
+
+axios.get("https://dummyjson.com/recipes/tags")
+    .then((resp) => {
+        //recupero dell'array dei tag dal srv
+        const tagsArray = resp.data;
+        let tagsRecipeHtmlSelectList = "";
+        tagsArray.forEach(curTag => {
+            return tagsRecipeHtmlSelectList += `<option value="${curTag}">${curTag}</option>`;
+        });
+        selectElem.innerHTML = tagsRecipeHtmlSelectList;
+    })
+
+
+/**
+ * 
+*/
+function getRecipeByTagGrid(selectedTag) {
+    axios.get(`https://dummyjson.com/recipes/tag/${selectedTag}`).then((resp) => {
+        const recipes = resp.data.recipes;
+        console.log("[DEBUG] recipes", recipes);
+        let cards = "";
+        recipes.forEach((curRecipe) => {
+            cards += getSingleCard(curRecipe);
+        });
+        gridElem.innerHTML = cards;
+    });
+}
+
+function getSingleCard({ image, name, tags, instructions }) {
+    let tagsHtml = "";
+
+    tags.forEach((curTag) => {
+        tagsHtml += `<span class="badge rounded-pill text-bg-warning">${curTag}</span>`;
+    });
+
+    return `
+      <div class="col">
+        <div class="card">
+          <img
+          max-height=200px
+            src="${image}"
+            class="card-img-top"
+            alt="Immagine di ${name}"
+          />
+          <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <div class="mb-2">
+              ${tagsHtml}
+              <p>${instructions[0].slice(0, 30)} ...</p>
+            </div>
+          </div>
+        </div> 
+      </div>
+    `;
+}
+
+formElem.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const tagSelected = selectElem.value;
+
+    getRecipeByTagGrid(tagSelected);
+
+})
