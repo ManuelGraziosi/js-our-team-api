e976beac35dc87c59e4119beb1f7316f47fc5ad8
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-10-22 19:54:05 +0200

    fix: card dimension with fixed size now

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -10,41 +10,46 @@ const gridElem = document.getElementById("grid");
 const formElem = document.getElementById("form");
 
 axios.get("https://dummyjson.com/recipes/tags")
-    .then((resp) => {
-        //recupero dell'array dei tag dal srv
-        const tagsArray = resp.data;
-        let tagsRecipeHtmlSelectList = "";
-        tagsArray.forEach(curTag => {
-            return tagsRecipeHtmlSelectList += `<option value="${curTag}">${curTag}</option>`;
-        });
-        selectElem.innerHTML = tagsRecipeHtmlSelectList;
-    })
+  .then((resp) => {
+    //recupero dell'array dei tag dal srv
+    const tagsArray = resp.data;
+    let tagsRecipeHtmlSelectList = "";
+    tagsArray.forEach(curTag => {
+      return tagsRecipeHtmlSelectList += `<option value="${curTag}">${curTag}</option>`;
+    });
+    selectElem.innerHTML = tagsRecipeHtmlSelectList;
+  })
 
 
 /**
  * 
 */
 function getRecipeByTagGrid(selectedTag) {
-    axios.get(`https://dummyjson.com/recipes/tag/${selectedTag}`).then((resp) => {
-        const recipes = resp.data.recipes;
-        console.log("[DEBUG] recipes", recipes);
-        let cards = "";
-        recipes.forEach((curRecipe) => {
-            cards += getSingleCard(curRecipe);
-        });
-        gridElem.innerHTML = cards;
+  axios.get(`https://dummyjson.com/recipes/tag/${selectedTag}`).then((resp) => {
+    const recipes = resp.data.recipes;
+    console.log("[DEBUG] recipes", recipes);
+    let cards = "";
+    recipes.forEach((curRecipe) => {
+      cards += getSingleCard(curRecipe);
     });
+    gridElem.innerHTML = cards;
+  });
 }
 
+/**
+ * descrizione: generazione della singola card in html formattata secondo le richieste
+ * @param {*} param0 
+ * @returns 
+ */
 function getSingleCard({ image, name, tags, instructions }) {
-    let tagsHtml = "";
+  let tagsHtml = "";
 
-    tags.forEach((curTag) => {
-        tagsHtml += `<span class="badge rounded-pill text-bg-warning">${curTag}</span>`;
-    });
+  tags.forEach((curTag) => {
+    tagsHtml += `<span class="badge rounded-pill text-bg-warning">${curTag}</span>`;
+  });
 
-    return `
-      <div class="col">
+  return `
+      <div class="col-3">
         <div class="card">
           <img
           max-height=200px
@@ -64,10 +69,13 @@ function getSingleCard({ image, name, tags, instructions }) {
     `;
 }
 
+/**
+ * Aggiunta dell'evento submit al form per generare la griglia in base al tag scelto
+ */
 formElem.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const tagSelected = selectElem.value;
+  event.preventDefault();
+  const tagSelected = selectElem.value;
 
-    getRecipeByTagGrid(tagSelected);
+  getRecipeByTagGrid(tagSelected);
 
 })
